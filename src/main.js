5ce5a875c5faffa80bd61d99e4647e7b591a0b47
Jorge Hernández <r3b0rn.jhs@gmail.com>
2018-05-22 09:24:57 +0100

    GREEN: test 'should convert numbers to Roman Numerals'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 function application(){
 
+    let numeral = function (value, numeral) {
+        return {value: value, numeral: numeral};
+    }
     let numerals = [
-        {value: 4, numeral: "IV"},
-        {value: 1, numeral: "I"}
+        numeral(1000,"M"),
+        numeral(900,"CM"),
+        numeral(500,"D"),
+        numeral(400,"CD"),
+        numeral(100,"C"),
+        numeral(90,"XC"),
+        numeral(50,"L"),
+        numeral(40,"XL"),
+        numeral(10,"X"),
+        numeral(9,"IX"),
+        numeral(5,"V"),
+        numeral(4,"IV"),
+        numeral(1,"I")
     ];
     function fromNumber(number) {
         let result = "";
