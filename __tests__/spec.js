5688f4532c2d12669d9893a6f95b8458b0a2d1cc
Jorge Hernández <r3b0rn.jhs@gmail.com>
2018-05-22 09:03:16 +0100

    Eliminated Unnecessary code

--- __tests__/spec.js
+++ __tests__/spec.js
@@ -1,19 +1,4 @@
-const fs = require('fs');
-const path = require('path');
-const chai = require('chai');
 const application = require('../src/main');
-chai.expect();
-
-function loadTemplate(filepath, onLoad) {
-    const filePath = path.join(__dirname, filepath);
-    fs.readFile(filePath, {encoding: 'utf-8'}, function (err, data) {
-        if (!err) {
-            onLoad(data);
-        } else {
-            console.log(err);
-        }
-    });
-}
 
 describe('RomanNumerals',function () {
     it('should convert numbers to Roman Numerals', function () {
