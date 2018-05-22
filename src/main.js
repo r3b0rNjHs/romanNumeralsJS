f144fc5f7abb690aaf9b1e9a37342ae59851adb3
Jorge Hernández <r3b0rn.jhs@gmail.com>
2018-05-22 09:12:28 +0100

    REFACTOR : function fromNumber()

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 function application(){
 
+    let numerals = [
+        {value: 4, numeral: "IV"},
+        {value: 1, numeral: "I"}
+    ];
     function fromNumber(number) {
         let result = "";
+        numerals.forEach(function (actualNumber) {
+            while (number >= actualNumber.value){
+                result += actualNumber.numeral;
+                number -= actualNumber.value;
+            }
 
-        while(number > 0){
-            result += "I";
-            --number;
-        }
+        });
         return result;
     }
 
